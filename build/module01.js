0ad05f3270f68e5eeb557fab77138308c0d02543
André Torres <andretorresjava@gmail.com>
2022-10-20 16:56:54 -0500

    Enumerations exercise

--- build/module01.js
+++ build/module01.js
@@ -3,3 +3,11 @@ function addNumbers(x, y) {
     return x + y;
 }
 console.log(addNumbers(3, 6));
+var ContractStatus;
+(function (ContractStatus) {
+    ContractStatus[ContractStatus["Permanent"] = 0] = "Permanent";
+    ContractStatus[ContractStatus["Temp"] = 1] = "Temp";
+    ContractStatus[ContractStatus["Apprentice"] = 2] = "Apprentice";
+})(ContractStatus || (ContractStatus = {}));
+let employeeStatus = ContractStatus.Temp;
+console.log(employeeStatus);
