5335d177352fe48380513b792426fdd4d9258260
André Torres <andretorresjava@gmail.com>
2022-10-23 17:51:03 -0500

    unknown example

--- Module 02 Exercise/practice.ts
+++ Module 02 Exercise/practice.ts
@@ -8,6 +8,18 @@ let employeeStatus: ContractStatus = ContractStatus.Temp;
 console.log(employeeStatus);
 console.log(ContractStatus[employeeStatus]);
 
-let randomValue: any = 10;
-randomValue = 'Mateo';   // OK
-randomValue = true;      // OK
+// let randomValue: any = 10;
+// randomValue = 'Mateo';   // OK
+// randomValue = true;      // OK
+
+let randomValue: unknown = 10;
+randomValue = true;
+randomValue = 'Mateo';
+
+console.log(randomValue.name);
+randomValue();
+randomValue.toUppercase();
+
+// Nota:
+// any omite las comprobaciones en tiempo de compilación y el objeto se evalúa en tiempo de ejecución.
+// Las aserciones de tipos tienen dos formatos. Una es la sintaxis de as y la otra es la sintaxis de "corchetes angulares".
--- build/Module 02 Exercise/practice.js
+++ build/Module 02 Exercise/practice.js
@@ -8,6 +8,15 @@ var ContractStatus;
 let employeeStatus = ContractStatus.Temp;
 console.log(employeeStatus);
 console.log(ContractStatus[employeeStatus]);
+// let randomValue: any = 10;
+// randomValue = 'Mateo';   // OK
+// randomValue = true;      // OK
 let randomValue = 10;
-randomValue = 'Mateo'; // OK
-randomValue = true; // OK
+randomValue = true;
+randomValue = 'Mateo';
+console.log(randomValue.name);
+randomValue();
+randomValue.toUppercase();
+// Nota:
+// any omite las comprobaciones en tiempo de compilación y el objeto se evalúa en tiempo de ejecución.
+// Las aserciones de tipos tienen dos formatos. Una es la sintaxis de as y la otra es la sintaxis de "corchetes angulares".
