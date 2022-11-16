6436c85b7f2443abbe7d7c0496853424008c58b4
André Torres <andretorresjava@gmail.com>
2022-11-16 14:27:22 -0500

    Extension of an interface in TypeScript

--- Module 03 Exercise/exercise.ts
+++ Module 03 Exercise/exercise.ts
@@ -1,15 +1,46 @@
 interface IceCream {
     flavor: string;
     scoops: number;
+    instructions?: string;
 }
 
-let myIceCream: IceCream = {
-    flavor: 'lemon',
-    scoops: 43
+// let myIceCream: IceCream = {
+//     flavor: 'vanilla',
+//     scoops: 2
+// }
+
+// console.log(myIceCream.scoops);
+
+// function tooManyScoops(dessert: IceCream){
+//     if(dessert.scoops >= 4){
+//         return dessert.scoops + 'is too many scoops!';
+//     }else{
+//         return 'Your order will be ready soon!';
+//     }
+// }
+
+// console.log(tooManyScoops({flavor: 'vanilla', scoops: 5}));
+
+interface Sundae extends IceCream{
+    sauce: 'chocolate' | 'caramel' | 'strawberry';
+    nuts?: boolean;
+    whippedCream?: boolean;
+    instructions?: boolean; 
 }
 
-console.log(myIceCream.scoops);
-
-function tooManyScoops(dessert: IceCream){
-    
+let myIceCream: Sundae = {
+    flavor: 'vanilla',
+    scoops: 2, 
+    sauce: 'caramel',
+    nuts: true   
 }
+
+function tooManyScoops(dessert: Sundae){
+    if(dessert.scoops >= 4){
+        return dessert.scoops + 'is too many scoops!';
+    }else{
+        return 'Your order will be ready soon!';
+    }
+}
+
+console.log(tooManyScoops({flavor: 'vanilla', scoops: 5, sauce: 'caramel'}));
