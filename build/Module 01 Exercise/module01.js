1a09299976758021a0bdf05ffc2fcd1a8dae841f
André Torres <andretorresjava@gmail.com>
2022-11-29 12:18:26 -0500

    Creating functions in TypeScript

--- build/Module 01 Exercise/module01.js
+++ build/Module 01 Exercise/module01.js
@@ -1,5 +1,5 @@
 "use strict";
-function addNumbers(x, y) {
-    return x + y;
-}
-console.log(addNumbers(3, 6));
+// function addNumbers(x: number, y: number) {
+//     return x + y;
+// }
+// console.log(addNumbers(3, 6));
