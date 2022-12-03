e738741ffbb1a77817eb88cbd0cb22f1ef265ee7
André Torres <andretorresjava@gmail.com>
2022-12-02 20:33:42 -0500

    Anonymous functions

--- build/Module 04 Exercise/exercise01.js
+++ build/Module 04 Exercise/exercise01.js
@@ -8,3 +8,14 @@ let addNumbers = function (x, y) {
     return x + y;
 };
 addNumbers(1, 2);
+let total = function (input) {
+    let total = 0;
+    for (let i = 0; i < input.length; i++) {
+        if (isNaN(input[i])) {
+            continue;
+        }
+        total += Number(input[i]);
+    }
+    return total;
+};
+console.log(total([1, 2, 3]));
