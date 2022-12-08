43fd36ac04031460bbf748783ed1ad54e49ad43a
André Torres <andretorresjava@gmail.com>
2022-12-08 15:16:52 -0500

    Arrow functions

--- Module 04 Exercise/exercise01.ts
+++ Module 04 Exercise/exercise01.ts
@@ -30,3 +30,14 @@ let addNumbers1 = function (x: number, y: number): number {
  
  // Arrow function
  let addNumbers2 = (x: number, y: number): number => x + y;
+
+ let total2 = (input: number[]): number => {
+    let total: number =  0;
+    for(let i = 0; i < input.length; i++) {
+        if(isNaN(input[i])) {
+            continue;
+        }
+        total += Number(input[i]);
+    }
+    return total;
+}
