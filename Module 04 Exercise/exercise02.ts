abcac69fe9ae8b2d5ee1abea00d549b63ca95e8d
André Torres <andretorresjava@gmail.com>
2022-12-12 15:13:08 -0500

    Function creation exercise

--- Module 04 Exercise/exercise02.ts
+++ Module 04 Exercise/exercise02.ts
@@ -3,3 +3,15 @@ displayAlert(567);
 function displayAlert(message: string | number) {
     alert('The message is ' + message);
 }
+
+function sum(input) {
+    let total =  0;
+    for(let count = 0; count < input.length; count++) {
+        if(isNaN(input[count])){
+            continue;
+        }
+        total += Number(input[count]);
+    }
+    return total;
+}
+sum(23);
