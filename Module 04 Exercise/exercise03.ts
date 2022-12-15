29b1f79d838ca0472ac4018e405a4b32460c8d3d
André Torres <andretorresjava@gmail.com>
2022-12-15 14:29:19 -0500

    Default parameters, REST parameters, and deconstructed object parameters.

--- Module 04 Exercise/exercise03.ts
+++ Module 04 Exercise/exercise03.ts
@@ -7,13 +7,49 @@
 //  addNumbers(1);    // Returns an error
 
 
-function addNumbers (x: number, y?: number): number {
-    if(y === undefined) {
-        return x;
-    }else {
-        return x + y;
-    }
+// function addNumbers (x: number, y?: number): number {
+//     if(y === undefined) {
+//         return x;
+//     }else {
+//         return x + y;
+//     }
+// }
+
+// addNumbers(1, 2); // Returns 3
+// addNumbers(1);    // Returns 1
+
+
+function addNumbers(x: number, y = 25): number{
+    return x + y;
 }
 
 addNumbers(1, 2); // Returns 3
-addNumbers(1);    // Returns 1
+addNumbers(1); // Returns 26
+
+
+let addAllNumbers = (firstNumber: number, ...restOfNumbers: number[]): number => {
+    let total: number = firstNumber;
+    for(let counter = 0; counter < restOfNumbers.length; counter ++){
+        if(isNaN(restOfNumbers[counter])){
+            continue;
+        }
+        total += Number(restOfNumbers[counter]);
+    }
+    return total;
+}
+
+addAllNumbers(1, 2, 3, 4, 5, 6, 7);  // returns 28
+addAllNumbers(2);                    // returns 2
+// addAllNumbers(2, 3, "three");        // flags error due to data type at design time, returns 5
+
+
+interface Message{
+    text: string;
+    sender: string;
+}
+
+function displayMessage({text, sender}: Message){
+    console.log(`Message from ${sender}: ${text}`);
+}
+
+displayMessage({sender: 'André', text: 'hello, world'});
--- build/Module 04 Exercise/exercise03.js
+++ build/Module 04 Exercise/exercise03.js
@@ -5,13 +5,33 @@
 // }
 //  addNumbers(1, 2); // Returns 3
 //  addNumbers(1);    // Returns an error
-function addNumbers(x, y) {
-    if (y === undefined) {
-        return x;
-    }
-    else {
-        return x + y;
-    }
+// function addNumbers (x: number, y?: number): number {
+//     if(y === undefined) {
+//         return x;
+//     }else {
+//         return x + y;
+//     }
+// }
+// addNumbers(1, 2); // Returns 3
+// addNumbers(1);    // Returns 1
+function addNumbers(x, y = 25) {
+    return x + y;
 }
 addNumbers(1, 2); // Returns 3
-addNumbers(1); // Returns 1
+addNumbers(1); // Returns 26
+let addAllNumbers = (firstNumber, ...restOfNumbers) => {
+    let total = firstNumber;
+    for (let counter = 0; counter < restOfNumbers.length; counter++) {
+        if (isNaN(restOfNumbers[counter])) {
+            continue;
+        }
+        total += Number(restOfNumbers[counter]);
+    }
+    return total;
+};
+addAllNumbers(1, 2, 3, 4, 5, 6, 7); // returns 28
+addAllNumbers(2); // returns 2
+function displayMessage({ text, sender }) {
+    console.log(`Message from ${sender}: ${text}`);
+}
+displayMessage({ sender: 'André', text: 'hello, world' });
