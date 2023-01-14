1ee4f6fbfdfa79d7e887d0eb0042520277f5f684
André Torres <andretorresjava@gmail.com>
2023-01-14 16:48:57 -0500

    Exercise: Creating a class

--- Module 05/exercise01.ts
+++ Module 05/exercise01.ts
@@ -1,3 +1,21 @@
 class Car{
-    
+    // Properties
+    _make: string;
+    _color: string;
+    _doors: number;
+
+    // Constructor
+    constructor(make: string, color: string, doors = 4) {
+        this._make = make;
+        this._color = color;
+        this._doors = doors;
+    }
+
+    // Accessors
+    get make() {
+        return this._make;
+    }
+
+    // Methods
 }
+
--- build/Module 05/exercise01.js
+++ build/Module 05/exercise01.js
@@ -1,3 +1,13 @@
 "use strict";
 class Car {
+    // Constructor
+    constructor(make, color, doors = 4) {
+        this._make = make;
+        this._color = color;
+        this._doors = doors;
+    }
+    // Accessors
+    get make() {
+        return this._make;
+    }
 }
